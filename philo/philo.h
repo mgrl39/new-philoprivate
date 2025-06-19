13dff2a4aff4fcf1fd63877075fe98e7502419b6
mgrl39 <170405410+mgrl39@users.noreply.github.com>
2025-06-19 03:27:25 +0200

    solve error .h and add atoi in utils

--- philo/philo.h
+++ philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: meghribe <meghribe@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/19 01:24:08 by meghribe          #+#    #+#             */
-/*   Updated: 2025/06/19 02:26:31 by meghribe         ###   ########.fr       */
+/*   Updated: 2025/06/19 03:26:42 by meghribe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,19 @@
 # define RED	"\033[38;5;203m"
 # define GOLD	"\033[38;5;220m"
 # define GREEN	"\033[38;5;120m"
+
+
+/**
+ * number_of_philosophers						-	
+ * time_to_die									-
+ * time_to_eat									-
+ * time_to_sleep								-
+ * number_of_times_each_philosopher_must_eat	-
+ */
+typedef struct s_philo
+{
+	int	id;
+}	t_philo;
 
 # define MSG_VALID_ARGS "number_of_philosophers time_to_die time_to_eat\
  time_to_sleep [number_of_times_each_philosopher_must_eat]"
---
+++ philo/utils.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: meghribe <meghribe@student.42barcelona.co  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/19 03:26:15 by meghribe          #+#    #+#             */
+/*   Updated: 2025/06/19 03:27:13 by meghribe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+int	ft_atoi(const char *nptr)
+{
+	size_t	i;
+	int		res;
+	int		mult;
+
+	i = 0;
+	res = 0;
+	mult = 1;
+	while (nptr[i] == 32 || (nptr[i] >= 9 && nptr[i] <= 13))
+		i++;
+	if (nptr[i] == '-' || nptr[i] == '+')
+		if (nptr[i++] == '-')
+			mult = -1;
+	while (nptr[i] >= 48 && nptr[i] <= 57)
+		res = res * 10 + (nptr[i++] - 48);
+	return (res * mult);
+}
