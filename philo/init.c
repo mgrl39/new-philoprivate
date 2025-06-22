581a3a6610928ae74296260c88da80c785dd6089
mgrl39 <170405410+mgrl39@users.noreply.github.com>
2025-06-22 07:37:28 +0200

    add init_philos method

--- philo/init.c
+++ philo/init.c
@@ -6,7 +6,7 @@
 /*   By: meghribe <meghribe@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/22 07:01:23 by meghribe          #+#    #+#             */
-/*   Updated: 2025/06/22 07:01:41 by meghribe         ###   ########.fr       */
+/*   Updated: 2025/06/22 07:37:19 by meghribe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,5 +30,25 @@ int	init_mutexes(t_data *data)
 			|| pthread_mutex_init(&data->locks.meal_lock, NULL) \
 			|| pthread_mutex_init(&data->locks.death_lock, NULL))
 		return (ft_error(MSG_MALLOC_ERR));
+	return (0);
+}
+
+int	init_philos(t_data *data)
+{
+	int	i;
+
+	data->philos = malloc(sizeof(t_philo) * data->num_philos);
+	if (!data->philos)
+		return (ft_error(MSG_MALLOC_ERR));
+	i = 0;
+	memset(data->philos, 0, sizeof(t_philo) * data->num_philos);
+	while (i < data->num_philos)
+	{
+		data->philos[i].id = i + 1;
+		data->philos[i].data = data;
+		data->philos[i].forks[LEFT] = &data->locks.forks[i];
+		data->philos[i].forks[RIGHT] = &data->locks.forks[(i + 1) % data->num_philos];
+		i++;
+	}
 	return (0);
 }
