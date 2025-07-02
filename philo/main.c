27e40a23997f1fecd4725564e8dc38a201d9c7d7
mgrl39 <170405410+mgrl39@users.noreply.github.com>
2025-07-02 21:55:55 +0200

    from monitoring.c to monitor.c

--- philo/main.c
+++ philo/main.c
@@ -6,7 +6,7 @@
 /*   By: meghribe <meghribe@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/30 19:43:06 by meghribe          #+#    #+#             */
-/*   Updated: 2025/07/02 21:42:30 by meghribe         ###   ########.fr       */
+/*   Updated: 2025/07/02 21:55:24 by meghribe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,12 @@
  * The main is a TL;DR of the program
  * We are gonna check if the input given at the command line is correct
  * we are going to kick in the Machinery 
- * Otherwise we're going to prompt the user to please feed me with correct prompt.
+ * Otherwise we're going to prompt the user 
+ * to please feed me with correct prompt.
  *
  * ./philo 5 800 200 200 [5]
- * ./philo philosophers time_to_die time_to_eat time_to_sleep potencially_number_or_meals
+ * ./philo philosophers time_to_die time_to_eat time_to_sleep 
+ * potencially_number_or_meals
  */
 int	main(int argc, char *argv[])
 {
@@ -35,6 +37,7 @@ int	main(int argc, char *argv[])
 		clean(&table);
 	}
 	else
+		// TODO CHANGE THIS
 		error_exit("Wrong input:\n"BLUE"Correct is ./philo 5 800 200 200 [5]"RESET);
 	return (0);
 }
