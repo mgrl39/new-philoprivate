1f955e117d5917d5f022afb5aa91571ae46c1d25
mgrl39 <170405410+mgrl39@users.noreply.github.com>
2025-07-03 13:54:21 +0200

    add print.c and monitor.c

---
+++ philo/monitor.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   monitor.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: meghribe <meghribe@student.42barcelon      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/03 12:50:33 by meghribe          #+#    #+#             */
+/*   Updated: 2025/07/03 13:51:34 by meghribe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+static bool philo_died(t_philo *philo)
+{
+	long	elapsed;
+	long	t_to_die;
+
+	if (get_bool(&philo->philo_mutex, &philo->full))
+		return (false);
+	elapsed = gettime(MILLISECOND);
+	elapsed -= get_long(&philo->philo_mutex, &philo->last_meal_time);
+	t_to_die = philo->table->time_to_die / 1e3;
+	if (elapsed > t_to_die)
+		return (true);
+	return (false);
+
+}
+
+void	*monitor_dinner(void *data)
+{
+	int	i;
+	t_table	*table;
+
+	table = (t_table *)data;
+	// make sure all philos running
+	// spinlock till all thread run
+	while (!all_threads_running(&table->table_mutex, &table->threads_running_nbr,
+			table->philo_nbr))
+		;
+	// constantly check time to die
+	while (!simulation_finished(table))
+	{
+		i = -1;
+		while (++i < table->philo_nbr && !simulation_finished(table))
+		{
+			if (philo_died(table->philos + i))
+			{
+				set_bool(&table->table_mutex, &table->end_simulation, true);
+				write_status(DIED, table->philos + i, DEBUG_MODE);
+			}
+		}
+	}
+	return (NULL);
+}
---
+++ philo/print.c
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   print.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: meghribe <meghribe@student.42barcelon      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/03 12:50:39 by meghribe          #+#    #+#             */
+/*   Updated: 2025/07/03 13:48:33 by meghribe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+#include <stdio.h>
+
+/**
+ * [time_ms] [philo_id] [action]
+ *
+ * thread safe
+ * write_mutex
+ */
+
+static void	write_status_debug(t_philo_status status, t_philo *philo,
+			long elapsed)
+{
+	if (TAKE_FIRST_FORK == status && !simulation_finished(philo->table))
+		printf(BOLD"%6ld"RESET
+			BLUE" %d has taken the 1 fork\t\t\tn [ %d ]\n"RESET,
+		       	elapsed, philo->id, philo->second_fork->fork_id);
+	else if (TAKE_SECOND_FORK == status && !simulation_finished(philo->table))
+		printf(BOLD"%6ld"RESET
+			RED" %d has taken the 2 fork\n\t\t\tn [ %d ]\n"RESET,
+		       	elapsed, philo->id, philo->second_fork->fork_id);
+	else if (EATING == status && !simulation_finished(philo->table))
+		printf(BOLD"%6ld"RESET" %d is eating\t\t\tn [ %ld ]\n"RESET,
+			elapsed, philo->id, philo->meals_counter);
+	else if (SLEEPING == status && !simulation_finished(philo->table))
+		printf(BOLD"%6ld"RESET" %d is sleeping\n", elapsed, philo->id);
+	else if (THINKING == status && !simulation_finished(philo->table))
+		printf(BOLD"%6ld"RESET" %d is thinking\n", elapsed, philo->id);
+	else if (DIED == status)
+		printf(RED"\t\t %6ld %d died \n"RESET, elapsed, philo->id);
+}
+
+void	write_status(t_philo_status status, t_philo *philo, bool debug)
+{
+	long	elapsed;
+
+	elapsed = gettime(MILLISECOND);
+	elapsed -= philo->table->start_simulation;
+	if (philo->full) // TODO Do it safe.
+		return ;
+	safe_mutex_handle(&philo->table->write_mutex, LOCK);
+	if (debug)
+		write_status_debug(status, philo, elapsed);
+	else
+	{
+		if ((TAKE_FIRST_FORK == status || TAKE_SECOND_FORK == status)
+				&& !simulation_finished(philo->table))
+			printf(BOLD"%-6ld"RESET"%d has taken a fork\n", elapsed, philo->id);
+		else if (EATING == status && !simulation_finished(philo->table))
+			printf(BOLD"%-6ld"RESET"%d is eating\n", elapsed, philo->id);
+		else if (SLEEPING == status && !simulation_finished(philo->table))
+			printf(BOLD"%-6ld"RESET"%d is sleeping\n", elapsed, philo->id);
+		else if (THINKING == status && !simulation_finished(philo->table))
+			printf(BOLD"%-6ld"RESET"%d is thinking\n", elapsed, philo->id);
+		else if (DIED == status)
+			printf(RED"%-6ld""%d died\n"RESET, elapsed, philo->id);
+	}
+	safe_mutex_handle(&philo->table->write_mutex, UNLOCK);
+}
+
+void	print_usage(char *argv[])
+{
+	(void)argv;
+}
