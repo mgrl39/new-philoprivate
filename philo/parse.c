93b657aa9808ee17fe5680c816af3d1bff382917
mgrl39 <170405410+mgrl39@users.noreply.github.com>
2025-07-04 18:50:42 +0200

    renamed parsing.c to parse.c and movee functions to another file bc norme is crying

--- philo/parse.c
+++ philo/parse.c
@@ -1,31 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   parsing.c                                          :+:      :+:    :+:   */
+/*   parse.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: meghribe <meghribe@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/30 19:49:54 by meghribe          #+#    #+#             */
-/*   Updated: 2025/07/04 18:36:24 by meghribe         ###   ########.fr       */
+/*   Updated: 2025/07/04 18:50:09 by meghribe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 #include <limits.h>
 #include <stdio.h>
-
-void	print_argument_error(int error, const char *arg, const char *param_name)
-{
-	if (error == ERR_NOT_DIGIT)
-		printf(RED MSG_ERR_NOT_DIGIT RESET "\n", arg);
-	else if (error == ERR_NEGATIVE)
-		printf(RED MSG_ERR_NEGATIVE RESET "\n", arg);
-	else if (error == ERR_OVERFLOW)
-		printf(RED MSG_ERR_OVERFLOW RESET "\n", arg, INT_MAX);
-	else if (error == ERR_ZERO_VALUE)
-		printf(RED "Error: %s (%s) cannot be zero." RESET "\n", \
-		param_name, arg);
-}
 
 static inline int	ft_isdigit(char c)
 {
@@ -125,20 +112,4 @@ int	process_arguments(t_table *table, char *av[])
 		return (1);
 	}
 	return (0);
-}
-
-// TODO: CAMBIAR A FT_PUTSTR_FD o A FT_ERROR O A OTRA COSA...
-void	print_parsing_error(int error_code, const char *arg)
-{
-	if (error_code == ERR_NOT_DIGIT)
-		printf(RED MSG_ERR_NOT_DIGIT RESET "\n", arg);
-	else if (error_code == ERR_NEGATIVE)
-		printf(RED MSG_ERR_NEGATIVE RESET "\n", arg);
-	else if (error_code == ERR_OVERFLOW)
-		printf(RED "Error: '%s' is too large. maximum \
-			allowed value is %d." RESET "\n", arg, INT_MAX);
-	else if (error_code == ERR_ZERO_VALUE)
-		printf(RED "Error: '%s' cannot be zero." RESET "\n", arg);
-	else if (error_code == ERR_TIMESTAMP)
-		printf(RED MSG_ERR_TIMESTAMP RESET "\n");
 }
