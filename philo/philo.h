f55468dfe2566b9e0c6536cc00fe516786cb030a
mgrl39 <170405410+mgrl39@users.noreply.github.com>
2025-07-08 19:21:28 +0200

    simplifying error parsing
    - changed print_argument_error now only needs 1 argment
    - removed from parse_and_change the char *param
    - using ft_alert instead of A_ERROR and A_WARN

--- philo/philo.h
+++ philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: meghribe <meghribe@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/30 18:44:41 by meghribe          #+#    #+#             */
-/*   Updated: 2025/07/06 16:26:59 by meghribe         ###   ########.fr       */
+/*   Updated: 2025/07/08 19:15:16 by meghribe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,22 +40,13 @@
 # define ERR_ZERO_VALUE	-4
 # define ERR_TIMESTAMP	-5
 
-# define ERR_DIT	"Error '%s' contains non-numeric characters. \
-Please use only digits."
-# define ERR_NEG	"Error: '%s' is a negativee number. Please use only \
-positive values."
-# define ERR_LARGE	"Error: '%s' is too large. Maximum allowed value \
-is %d."
-# define ERR_ZERO	"Error: '%s' (%s) cannot be zero."
-# define ERR_TIME	"Error: all timings must be at least 60ms"
+# define ERR_DIT	"Error: Input contains non-numeric characters."
+# define ERR_NEG	"Error: Negative numbers are not allowed."
+# define ERR_LARGE	"Error: Number is too large."
+# define ERR_ZERO	"Error: Value cannot be zero."
+# define ERR_TIME	"Error: All timings must be at least 60ms."
 # define ERR_MALLOC	"Error: malloc"
 # define ERR_MUTEX	"Error: mutex"
-
-# define ARG_PHILOS 	"number of philosophers"
-# define ARG_DIE_TIME 	"time to die"
-# define ARG_EAT_TIME 	"time to eat"
-# define ARG_SLEEP_TIME "time to sleep"
-# define ARG_MEALS		"number of meals"
 
 # define MIN_TIMESTAMP	60e3
 
@@ -198,9 +189,7 @@ void	thinking(t_philo *philo, int pre_simulation);
 void	prevent_simultaneous_start(t_philo *philo);
 void	precise_usleep(long usec, t_table *table);
 void	print_argument_error(
-			int error,
-			const char *arg,
-			const char *param_name);
+		int error);
 void	wait_all_threads(t_table	*table);
 
 long	gettime(t_time_code	time_code);
