085e3174d620e2c262c4a5909b355cb11d0e3bb6
mgrl39 <170405410+mgrl39@users.noreply.github.com>
2025-07-11 21:08:18 +0200

    refactor comments styling

--- philo/routine.c
+++ philo/routine.c
@@ -6,17 +6,14 @@
 /*   By: meghribe <meghribe@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/01 21:21:28 by meghribe          #+#    #+#             */
-/*   Updated: 2025/07/10 02:07:39 by meghribe         ###   ########.fr       */
+/*   Updated: 2025/07/11 21:07:20 by meghribe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 #include <unistd.h>
 
-/**
- * 1) Fake to lock the fork.
- * 2) Sleep until the monitor will bust it.
- */
+// Fake to lock the fork and sleep until the monitor will bust it.
 void	*single_philo(void *arg)
 {
 	t_philo	*philo;
@@ -57,19 +54,17 @@ void	thinking(t_philo *philo, int pre_simulation)
 }
 
 /**
- * MOST IMPORTANT THING FOR A PHILOSOPHER
- * eat routine
- * 1) grab the forks: here first and second fork is handy
+ * Grab the forks: here first and second fork is handy
  * 	i dont worry about left right 
- * 2) eat: write eat, update last meal, update meals counter.
+ * Eat: write eat, update last meal, update meals counter.
  * 	eventyally int full
- * 3) release the forks
- * 2) Set the last meal time:
+ * Release the forks
+ * Set the last meal time:
  * Actual eating. is set the last meal time and we
  * are gonna do it immediately.
  * sleep the time requested.
  * if is true the philo is full
- 3) UNLOCK
+ * unlock
  */
 static int	eat(t_philo *philo)
 {
@@ -101,27 +96,21 @@ static int	eat(t_philo *philo)
 	return (0);
 }
 
-// here is goonna be the actual simulation of the dinner
 /*
  * 0) Wait all philos, synchro start
  * 1) endless loop philo
- */
-// set time last meal
-/**
+ * set time last meal
  * syncro with monitor
  * increase a table variable cunter, with al threads running
- */
-// desynchronizing philos
-/*
+ * desynchronizing philos
  * until the dinner is not finished
+ * 1) the philosopher has to check: I am full? 
+ * in that case i exit.
+ * 2) EAT
+ * sleep(philo);
+ * 3) SLEEP -> write the actual status (write_status) & precise usleep
+ * 4) THINK
  */
-// 1) the philosopher has to check: I am full?
-// in that case i exit.
-//
-// 2) EAT
-// sleep(philo);
-// 3) SLEEP -> write the actual status (write_status) & precise usleep
-// 4) THINK
 void	*dinner_simulation(void *data)
 {
 	t_philo	*philo;
@@ -147,34 +136,17 @@ void	*dinner_simulation(void *data)
 }
 
 /**
- * ./philo 5 800 200 200 [5]
- *
- * 0) If no meals, return -> [0]
- * 0.1) If only one philo -> we do an function for this situation.
- * 1) Create all threads, all philos
- * 2) Create a monitor thread -> thread that is searching for philsopher
- *	 which are death.
- * 3) Syncronize the beggining of the simulation -> everytime you call 
+ * If no meals, return -> [0]
+ * If only one philo -> function for this situation
+ * Create all threads (all philos)
+ * Create a monitor thread -> that is searching for philos which are death.
+ * Syncronize the beggining of the simulation -> everytime you call 
  * pthread_create the philosophers start to run.
- * 	I want every philo start simultaneously. We need synchronization 
+ * 	I want every philo start simultaneously. 
+ * 	We need synchronization 
  * 	thing to make all the philos start at the same time.
- * 4) JOIN everyone
- * back to main, clean
- * This is a while loop that is gonna be create all 
- * the threads, all the philosophers.
- * Every time this funciton is called immediately 
- * the thread starts running this dinner simulation function 
- * that we still have to do. 
- * So we need a synhronization thing to make 
- * all the philosophers start at the same time monitor
- * start of simulation
- * we need a chronometer, we need a function 
- * that is able to give us back the 
- * actual time. 
+ * join (wait) everyone.
  */
-// now all threads are ready!
-// we are going to join (wait for everyone)
-// If we manage to reach this line all philos are FULL.
 int	dinner_start(t_table *table)
 {
 	int	created;
